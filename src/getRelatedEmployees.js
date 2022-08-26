fee825aabc38e1f4d2d738a698a7c217b90b3dbc
Paim <victor-paim@hotmail.com>
2022-08-26 19:15:18 -0300

    .

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managers = data.employees.some((manager) => manager.managers.includes(id));
+  return managers;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees
+    .filter((empregados) => empregados.managers
+      .includes(managerId))
+    .map((empregados) => `${empregados.firstName} ${empregados.lastName}`); 
 }
 
 module.exports = { isManager, getRelatedEmployees };
