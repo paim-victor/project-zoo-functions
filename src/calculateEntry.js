d24bb9bf5e1021e6721ff2aaa27729a668f19ce5
Paim <victor-paim@hotmail.com>
2022-08-29 16:05:28 -0300

    finalizando função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui a
-}
+  let valorEntrada = {
+    child: (entrants.filter((person) => person.age < 18)).length,
+    adult: (entrants.filter((person) => person.age < 50 && person.age >= 18)).length,
+    senior: (entrants.filter((person) => person.age >= 50)).length,
+  }; return valorEntrada;
+};
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) 
+  return 0;
+  let todos = countEntrants(entrants);
+  let dinheiro = 0;
+  dinheiro += todos.adult * data.prices.adult;
+  dinheiro += todos.child * data.prices.child;
+  dinheiro += todos.senior * data.prices.senior;
+  return dinheiro;
 }
 
 module.exports = { calculateEntry, countEntrants };
