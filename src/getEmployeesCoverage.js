6572c60a8b42baf232fc3d8c0d79a5601d66bafe
Paim <victor-paim@hotmail.com>
2022-08-30 17:24:47 -0300

    finalizando e refatorando o requisito 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const semParametro = () => {
+  const animaisID = (id, parametro) => id.map((animal) => (data.species
+    .find((elemento) => elemento.id === animal))[parametro]);
+  const todos = data.employees.map((valor) => {
+    const informacoes = { id: valor.id };
+    informacoes.fullName = `${valor.firstName} ${valor.lastName}`;
+    informacoes.species = animaisID(valor.responsibleFor, 'name');
+    informacoes.locations = animaisID(valor.responsibleFor, 'location'); return informacoes;
+  }); return todos;
+};
+const getEmployeesCoverage = (pessoa) => {
+  if (!pessoa) return semParametro(); const empregado = semParametro()
+    .find((valor) => valor.fullName.includes(pessoa.name) || valor.id === pessoa.id);
+  if (empregado) return empregado; throw new Error('Informações inválidas');
+};
 
 module.exports = getEmployeesCoverage;
